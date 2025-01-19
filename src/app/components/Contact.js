d050b97e6b8c02b92f955b1ae41d946b9716bb06
Arya Jagadditha <68953724+DrRhea@users.noreply.github.com>
2025-01-19 16:39:58 +0700

    fix: all the client has requested

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -116,9 +116,9 @@ const Contact = () => {
             <div className="mt-8 flex justify-end">
               <button
                 type="submit"
-                className="rounded-md bg-gray-900 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
+                className="rounded-md bg-gray-900 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900 w-full"
               >
-                Send message
+                Contact Us
               </button>
             </div>
           </div>
