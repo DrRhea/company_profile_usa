e4257ce5e14da1007745db3c4bb1b86d9ce4b54b
Arya Jagadditha <68953724+DrRhea@users.noreply.github.com>
2025-01-20 00:13:19 +0700

    fix: mobile nav menu animation

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -173,7 +173,7 @@ export default function Navbar() {
                       to="services"
                       smooth={true}
                       duration={500}
-                      offset={calculateOffset(-100)}
+                      offset={calculateOffset(-90)}
                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800 cursor-pointer"
                       onClick={() => setMenuOpen(false)}
                     >
