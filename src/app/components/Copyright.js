95b919e834dc0961ac768b2ce70c8ae7e122a36b
Arya Jagadditha <68953724+DrRhea@users.noreply.github.com>
2025-01-23 13:57:01 +0700

    fix: name in the copyright section

--- src/app/components/Copyright.js
+++ src/app/components/Copyright.js
@@ -5,7 +5,8 @@ const Copyright = () => {
     <footer className="bg-gray-900">
       <div className="mx-auto max-w-7xl px-6 py-6 md:flex md:items-center md:justify-center lg:px-8">
           <div className="md:order-1 md:mt-0">
-              <p className="text-center text-sm leading-5 text-white">&copy;2010-2025 AHHM Corporations. All rights reserved.</p>
+              <p className="text-center text-sm leading-5 text-white">
+                &copy;2012-2025 AW Consulting. All rights reserved.</p>
           </div>
       </div>
     </footer>
